3a32acd6b88fbb664f85fc0be86c528da6e4718f
MikeHebner <45646471+MikeHebner@users.noreply.github.com>
2021-06-29 14:10:52 -0400

    Add files via upload

---
+++ inputValidation.py
@@ -0,0 +1,26 @@
+import pyinputplus as pyip
+import random
+import time
+
+numberOfQuestions = 10
+correctAnswers = 0
+for quesntionNumber in range(numberOfQuestions):
+    num1 = random.randint(0,9)
+    num2 = random.randint(0,9)
+
+    prompt = '#%s: %s x %s='%(quesntionNumber,num1,num2)
+
+    try:
+        pyip.inputStr(prompt, allowRegexes=['^%s$'%(num1*num2)],
+        blockRegexes=[('.*','Incorrect!')],
+        timeout=8,limit=3)
+    except pyip.TimeoutException:
+        print('Out of Time')
+    except pyip.RetryLimitException:
+        print('Out of tries')
+    else:
+        print('Correct')
+        correctAnswers +=1
+
+    time.sleep(1)
+    print('Score: %s/%s'%(correctAnswers,numberOfQuestions))
