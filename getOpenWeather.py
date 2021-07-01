5732980598da775ab4c48fd0b55821db888c5986
michaelhebner <mhebner@student.bridgew.edu>
2021-07-01 13:44:12 -0400

    booya

--- getOpenWeather.py
+++ getOpenWeather.py
@@ -1,6 +1,11 @@
-
-import json
 import requests
+
+
+def convertTemp(kTemp):
+    fTemp = ((kTemp - 273.15) * (9 / 5) + 32)
+    fTemp = round(fTemp, 2)
+    return fTemp
+
 
 class OpenWeatherMap:
     APPID = 'da055e0fdcebcdd212402b9b81768fab'
@@ -10,37 +15,33 @@ class OpenWeatherMap:
         self.json = {}
 
     def getZipcode(self):
-        url = self.url.format(appid=OpenWeatherMap.APPID,zipCode=input('Enter Zip Code '))
+        url = self.url.format(appid=OpenWeatherMap.APPID, zipCode=input('Enter Zip Code '))
         self.json = requests.get(url).json()
         return self.json
 
-    def K2F(kTemp):
-        fTemp = ((kTemp-273.15)*(9/5)+32)
-        fTemp = round(fTemp,2)
-        return fTemp
-
     def loadWeather(self):
-        
         weatherData = self.json
         w = weatherData['weather']
         tempData = (weatherData['main'])
 
         locName = weatherData['name']
         wState = w[0]['main']
-        wDescrip = (w[0]['description']).capitalize()
-        curTemp = OpenWeatherMap.K2F(tempData['temp'])
-        realFeel = OpenWeatherMap.K2F(tempData['feels_like'])
-        lowTemp = OpenWeatherMap.K2F(tempData['temp_min'])
-        highTemp = OpenWeatherMap.K2F(tempData['temp_max'])
+        wDescription = (w[0]['description']).capitalize()
+        curTemp = convertTemp(tempData['temp'])
+        realFeel = convertTemp(tempData['feels_like'])
+        lowTemp = convertTemp(tempData['temp_min'])
+        highTemp = convertTemp(tempData['temp_max'])
         humidity = tempData['humidity']
 
-        print('Current weather in %s:'%(locName))
-        print(wState,':',wDescrip)
+        print('Current weather in %s:' % (locName))
+        print(wState, '->', wDescription)
         print('Temp:', curTemp)
-        print('Humidity:',humidity,'%')
-        print('Feels like:',realFeel)
-        print('High:',highTemp,'| Low:', lowTemp)
+        print('Humidity:', humidity, '%')
+        print('Feels like:', realFeel)
+        print('High:', highTemp, '| Low:', lowTemp)
 
-x = OpenWeatherMap()
-x.getZipcode()
-x.loadWeather()
+
+while True:
+    x = OpenWeatherMap()
+    x.getZipcode()
+    x.loadWeather()
